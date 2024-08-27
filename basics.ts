1b504710c55c34b14ea71947b2847b24315c636f
HashDBrown <halkhateeb704@gmail.com>
2024-08-27 09:01:53 -0400

    difference between const and let. Why setting variables with type is bad practice.

--- basics.ts
+++ basics.ts
@@ -14,10 +14,17 @@ function add(num1: number, num2: number, showResult: boolean, phrase: string){
     }
 }
 
+//we don't have types here because typescript is smart enough to know that number1 is a number
+//this is called type inference
 const number1 = 10;
 const number2 = 2.8;
 const printResult = true;
 const phrase = 'Result is: ';
+
+//difference between const and let is that const is immutable
+//what is immutable? it means that the value of the variable cannot be changed
+
+//let n1 = 10; is bad practice because we are not using typescript to its full potential
 
 // const result = add(number1, number2, printResult);
 add(number1, number2, printResult, phrase);
