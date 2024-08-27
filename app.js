ad7ff84e871aae87116da2edbfd2374026c290e8
HashDBrown <halkhateeb704@gmail.com>
2024-08-26 23:14:26 -0400

    added typing to variables

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('Hello World');
+// types
+// numbers- all numbers, no difference between integers and floats
+// strings- all text values 'Hi', "Hi", `Hi`
+// boolean- true or false
+function add(num1, num2) {
+    console.log(typeof num1);
+    return num1 + num2;
+}
+var number1 = 10;
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
 console.log('Hello World');
+// types
+// numbers- all numbers, no difference between integers and floats
+// strings- all text values 'Hi', "Hi", `Hi`
+// boolean- true or false
+
+function add(num1: number, num2: number) :number{
+    console.log(typeof num1);
+    return num1 + num2;
+}
+
+const number1 = 10;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
