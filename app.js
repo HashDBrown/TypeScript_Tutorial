39c6404b8876124d256f1ea6b1579667c1506237
HashDBrown <halkhateeb704@gmail.com>
2024-08-27 21:03:27 -0400

    Went over Objects and starting Arrays

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-console.log('Hello World');
-// types
-// numbers- all numbers, no difference between integers and floats
-// strings- all text values 'Hi', "Hi", `Hi`
-// boolean- true or false
-function add(num1, num2, showResult, phrase) {
-    // console.log(typeof num1);
-    var result = num1 + num2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return num1 + num2;
-    }
-}
-var number1 = 10;
-var number2 = 2.8;
-var printResult = true;
-var phrase = 'Result is: ';
-// const result = add(number1, number2, printResult);
-add(number1, number2, printResult, phrase);
+//objects
+var person = {
+    name: 'Maximilian',
+    age: 30
+};
+console.log(person.name);
---
+++ app.ts
@@ -0,0 +1,23 @@
+//objects
+
+// {} is a type of object
+//typescrips notation for objects is key value pairs
+const person = {
+    name: 'Maximilian',
+    age: 30
+};
+
+//we can be more specific
+// const person2: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Maximilian',
+//     age: 30
+// };
+
+
+//Arrays in typescript
+//types of arrays can be flexible or strict
+
+console.log(person.name);
