461b7a3639fcef8bb9828b335284ddf0f2f26a79
HashDBrown <halkhateeb704@gmail.com>
2024-08-28 22:18:11 -0400

    arrays and for-loops

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 //objects
+// {} is a type of object
+//typescrips notation for objects is key value pairs
 var person = {
     name: 'Maximilian',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking'], //infered hobbies is an array of strings
+    role: [2, 'author'] //union type
 };
+person.role.push('admin');
+var favoriteActivities; //array of strings
+//array of type any can store any type of data
+// let favoriteActivities: any[];
+//we can be more specific
+// const person2: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Maximilian',
+//     age: 30
+// };
+//Arrays in typescript
+//types of arrays can be flexible or strict
+//for-loop
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase);
+    // console.log(hobby.map()); // !!! ERROR !!!
+}
 console.log(person.name);
--- app.ts
+++ app.ts
@@ -2,16 +2,24 @@
 
 // {} is a type of object
 //typescrips notation for objects is key value pairs
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]; //tuple
+}= {
     name: 'Maximilian',
     age: 30,
-    hobbies: ['Sports', 'Cooking'] //infered hobbies is an array of strings
+    hobbies: ['Sports', 'Cooking'], //infered hobbies is an array of strings
+    role: [2, 'author'] //union type
 };
+
+person.role.push('admin');
 
 let favoriteActivities: string[]; //array of strings
 
 //array of type any can store any type of data
-// let favoriteActivities: any[]; //array of strings
+// let favoriteActivities: any[];
 
 //we can be more specific
 // const person2: {
@@ -27,5 +35,9 @@ let favoriteActivities: string[]; //array of strings
 //types of arrays can be flexible or strict
 
 //for-loop
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase);
+    // console.log(hobby.map()); // !!! ERROR !!!
+}
 
 console.log(person.name);
