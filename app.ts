dae4861cd58c192fdf0afd657a2799c2cf0ac6bb
HashDBrown <halkhateeb704@gmail.com>
2024-08-28 22:05:56 -0400

    array with inferred or declared type

--- app.ts
+++ app.ts
@@ -4,8 +4,14 @@
 //typescrips notation for objects is key value pairs
 const person = {
     name: 'Maximilian',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking'] //infered hobbies is an array of strings
 };
+
+let favoriteActivities: string[]; //array of strings
+
+//array of type any can store any type of data
+// let favoriteActivities: any[]; //array of strings
 
 //we can be more specific
 // const person2: {
@@ -19,5 +25,7 @@ const person = {
 
 //Arrays in typescript
 //types of arrays can be flexible or strict
+
+//for-loop
 
 console.log(person.name);
